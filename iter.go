abcc06069cd2f366ca77a5ce4a3377e02dbb950e
chiyoi <chiyoi2140@outlook.com>
2023-11-28 12:24:42 +0800

    tidy

--- iter.go
+++ iter.go
@@ -1,249 +1,54 @@
 package iter
 
-const (
-	Continue = true
-	Break    = false
+import (
+	"errors"
+
+	"github.com/chiyoi/iter/res"
 )
 
-type Iterator[T any] interface {
-	Next() (T, bool)
-}
+var (
+	ErrStopIteration   = errors.New("stop iteration")
+	IsErrStopIteration = func(err error) bool {
+		return errors.Is(err, ErrStopIteration)
+	}
+)
 
-type IteratorFunc[T any] func() (T, bool)
+type Mapping[A, B any] func(A) (B, error)
 
-func (it IteratorFunc[T]) Next() (T, bool) {
-	return it()
-}
+type Folding[A, B any] func(B) func(A) (B, error)
 
-func Iter[T any](sli []T) Iterator[T] {
+type Iterator[A any] func() (A, error)
+
+func Iter[A any](sli []A) Iterator[A] {
 	i := 0
-	return IteratorFunc[T](func() (v T, ok bool) {
+	return func() (a A, err error) {
 		if i < len(sli) {
-			v, ok = sli[i], true
+			a = sli[i]
 			i++
 			return
 		}
-		var zero T
-		return zero, false
-	})
-}
-
-func Keys[K comparable, A any](m map[K]A) Iterator[K] {
-	keys := make([]K, len(m))
-	for k := range m {
-		keys = append(keys, k)
-	}
-	return Iter(keys)
-}
-
-func IterRev[T any](sli []T) Iterator[T] {
-	i := len(sli) - 1
-	return IteratorFunc[T](func() (v T, ok bool) {
-		if i >= 0 {
-			v, ok = sli[i], true
-			i--
-			return
-		}
-		var zero T
-		return zero, false
-	})
-}
-
-func Range(a, b int, step int) Iterator[int] {
-	if step == 0 || a <= b && step < 0 || a >= b && step > 0 {
-		return Empty[int]()
-	}
-	i := a
-	return IteratorFunc[int](func() (int, bool) {
-		if step > 0 && i >= b || step < 0 && i <= b {
-			return 0, false
-		}
-		i0 := i
-		i += step
-		return i0, true
-	})
-}
-
-func Empty[T any]() Iterator[T] {
-	return IteratorFunc[T](func() (T, bool) {
-		var zero T
-		return zero, false
-	})
-}
-
-func Once[T any](v T) Iterator[T] {
-	var flag bool
-	return IteratorFunc[T](func() (T, bool) {
-		if flag {
-			var zero T
-			return zero, false
-		}
-		flag = true
-		return v, true
-	})
-}
-
-func Repeat[T any](v T) Iterator[T] {
-	return IteratorFunc[T](func() (T, bool) {
-		return v, true
-	})
-}
-
-func Chain[T any](itA, itB Iterator[T]) Iterator[T] {
-	return IteratorFunc[T](func() (T, bool) {
-		t, ok := itA.Next()
-		if ok {
-			return t, true
-		}
-		return itB.Next()
-	})
-}
-
-func Zip[A, B any](itA Iterator[A], itB Iterator[B]) Iterator[ZipItem[A, B]] {
-	return IteratorFunc[ZipItem[A, B]](func() (ZipItem[A, B], bool) {
-		a, okA := itA.Next()
-		b, okB := itB.Next()
-		if !okA || !okB {
-			return ZipItem[A, B]{}, false
-		}
-		return ZipItem[A, B]{a, b}, true
-	})
-}
-
-func Map[A, B any](it Iterator[A], f func(A) B) Iterator[B] {
-	return IteratorFunc[B](func() (b B, ok bool) {
-		a, ok := it.Next()
-		if !ok {
-			return
-		}
-		return f(a), true
-	})
-}
-
-func Scan[T, St any](it Iterator[T], st St, f func(St, T) (St, bool)) Iterator[St] {
-	return IteratorFunc[St](func() (out St, ok bool) {
-		v, ok := it.Next()
-		if !ok {
-			return
-		}
-		st1, ok := f(st, v)
-		if !ok {
-			return st, false
-		}
-		st = st1
-		return st, true
-	})
-}
-
-func Take[T any](it Iterator[T], count int) Iterator[T] {
-	i := 0
-	return IteratorFunc[T](func() (v T, ok bool) {
-		if i < count {
-			i++
-			return it.Next()
-		}
+		err = ErrStopIteration
 		return
-	})
+	}
 }
 
-func Skip[T any](it Iterator[T], count int) Iterator[T] {
-	var flag bool
-	return IteratorFunc[T](func() (v T, ok bool) {
-		if !flag {
-			for i := 0; i < count; i++ {
-				_, ok = it.Next()
-				if !ok {
-					return
-				}
+func Map[A, B any](it Iterator[A], f Mapping[A, B]) Iterator[B] {
+	return func() (b B, err error) {
+		a, err := it()
+		return res.R(a, err, f)
+	}
+}
+
+func Collect[A any](it Iterator[A]) (ans []A, err error) {
+	for {
+		var a A
+		a, err = it()
+		if err != nil {
+			if IsErrStopIteration(err) {
+				err = nil
 			}
-			flag = true
-		}
-		return it.Next()
-	})
-}
-
-func Filter[T any](it Iterator[T], f func(T) bool) Iterator[T] {
-	return IteratorFunc[T](func() (v T, ok bool) {
-		v, ok = it.Next()
-		if !ok {
 			return
 		}
-		for !f(v) {
-			v, ok = it.Next()
-			if !ok {
-				return
-			}
-		}
-		return
-	})
-}
-
-func Last[T any](it Iterator[T]) (v T, ok bool) {
-	return Reduce(it, func(_, t T) T {
-		return t
-	})
-}
-
-func At[T any](it Iterator[T], i int) (v T, ok bool) {
-	if i < 0 {
-		return
+		ans = append(ans, a)
 	}
-	return Skip(it, i).Next()
-}
-
-func Reduce[T any](it Iterator[T], f func(T, T) T) (ans T, ok bool) {
-	ans, ok = it.Next()
-	if !ok {
-		return
-	}
-	for {
-		v, ok := it.Next()
-		if !ok {
-			break
-		}
-		ans = f(ans, v)
-	}
-	return
-}
-
-func Fold[St, T any](it Iterator[T], st St, f func(St, T) (St, bool)) St {
-	for {
-		v, ok := it.Next()
-		if !ok {
-			break
-		}
-		st1, ok := f(st, v)
-		if !ok {
-			return st
-		}
-		st = st1
-	}
-	return st
-}
-
-func Collect[T any](it Iterator[T]) (ans []T) {
-	for {
-		v, ok := it.Next()
-		if !ok {
-			break
-		}
-		ans = append(ans, v)
-	}
-	return
-}
-
-func Count[T any](it Iterator[T]) (count int) {
-	for {
-		_, ok := it.Next()
-		if !ok {
-			break
-		}
-		count++
-	}
-	return
-}
-
-type ZipItem[A, B any] struct {
-	A A
-	B B
 }
--- iter_test.go
+++ iter_test.go
@@ -1,264 +1,41 @@
 package iter
 
 import (
-	"fmt"
-	"reflect"
 	"strconv"
 	"testing"
+
+	"github.com/chiyoi/apricot/test"
 )
-
-func ExampleIter() {
-	fmt.Println(Collect(Iter([]int{1, 2, 3, 4, 5})))
-	// Output: [1 2 3 4 5]
-}
-
-func ExampleIterRev() {
-	fmt.Println(Collect(IterRev([]int{1, 2, 3, 4, 5})))
-	// Output: [5 4 3 2 1]
-}
-
-func TestIter(t *testing.T) {
-	for i, tc := range [][2][]int{
-		{{1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}},
-		{{0}, {0}},
-		{{}, nil},
-		{nil, nil},
-	} {
-		got := Collect(Iter(tc[0]))
-		if !reflect.DeepEqual(got, tc[1]) {
-			t.Errorf("Testcase %d: got %v, expect %v.", i, got, tc[1])
-		}
-	}
-}
-
-func TestIterRev(t *testing.T) {
-	for i, tc := range [][2][]int{
-		{{1, 2, 3, 4, 5}, {5, 4, 3, 2, 1}},
-		{{0}, {0}},
-		{{}, nil},
-		{nil, nil},
-	} {
-		got := Collect(IterRev(tc[0]))
-		if !reflect.DeepEqual(got, tc[1]) {
-			t.Errorf("Testcase %d: got %v, expect %v.", i, got, tc[1])
-		}
-	}
-}
-
-func ExampleRange() {
-	fmt.Println(Collect(Range(0, 10, 1)))
-	// Output: [0 1 2 3 4 5 6 7 8 9]
-}
-
-func TestRange(t *testing.T) {
-	for i, tc := range []struct {
-		a, b int
-		step int
-		out  []int
-	}{
-		{0, 10, 1, []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}},
-		{0, 10, 3, []int{0, 3, 6, 9}},
-		{10, 0, -3, []int{10, 7, 4, 1}},
-		{-3, 3, 1, []int{-3, -2, -1, 0, 1, 2}},
-		{0, 0, 1, nil},
-		{10, 0, 1, nil},
-	} {
-		out := Collect(Range(tc.a, tc.b, tc.step))
-		if !reflect.DeepEqual(out, tc.out) {
-			t.Errorf("Testcase %d: got %v, expect %v.", i, out, tc.out)
-		}
-	}
-}
-
-func ExampleEmpty() {
-	fmt.Println(Collect(Empty[int]()))
-	// Output: []
-}
-
-func ExampleRepeat() {
-	fmt.Println(Collect(Take(Repeat(3), 2)))
-	// Output: [3 3]
-}
-
-func TestEmptyOnceRepeatChainSkipTake(t *testing.T) {
-	for i, tc := range []struct {
-		x          int
-		n          int
-		skip, take int
-		out        []int
-	}{
-		{123, 1, 3, 3, []int{1, 1, 1}},
-		{123, 0, 0, 3, []int{123, 0, 0}},
-		{123, -1, -1, 3, []int{123, -1, -1}},
-		{123, 1, 3, 0, nil},
-		{123, 1, 3, -1, nil},
-	} {
-		out := Collect(Take(Skip(Chain(Empty[int](), Chain(Once(tc.x), Repeat(tc.n))), tc.skip), tc.take))
-		if !reflect.DeepEqual(out, tc.out) {
-			t.Errorf("Testcase %d: got %v, expect %v.", i, out, tc.out)
-		}
-	}
-}
-
-func TestZip(t *testing.T) {
-	for i, tc := range []struct {
-		a   []int
-		b   []string
-		out []ZipItem[int, string]
-	}{
-		{[]int{1, 2, 3}, []string{"a", "b", "c"}, []ZipItem[int, string]{{1, "a"}, {2, "b"}, {3, "c"}}},
-		{[]int{1, 2}, []string{"a", "b", "c"}, []ZipItem[int, string]{{1, "a"}, {2, "b"}}},
-		{[]int{1, 2, 3}, []string{"a", "b"}, []ZipItem[int, string]{{1, "a"}, {2, "b"}}},
-		{nil, []string{"a", "b", "c"}, nil},
-		{[]int{1, 2, 3}, nil, nil},
-	} {
-		out := Collect(Zip(Iter(tc.a), Iter(tc.b)))
-		if !reflect.DeepEqual(out, tc.out) {
-			t.Errorf("Testcase %d: got %v, expect %v.", i, out, tc.out)
-		}
-	}
-}
-
-func TestLast(t *testing.T) {
-	for i, tc := range []struct {
-		sli []int
-		out int
-		ok  bool
-	}{
-		{[]int{1, 2, 3}, 3, true},
-		{[]int{0}, 0, true},
-		{[]int{}, 0, false},
-		{nil, 0, false},
-	} {
-		out, ok := Last(Iter(tc.sli))
-		if out != tc.out || ok != tc.ok {
-			t.Errorf("Testcase %d: got (%v, %v), expect (%v, %v).", i, out, ok, tc.out, tc.ok)
-		}
-	}
-}
-
-func TestAt(t *testing.T) {
-	for i, tc := range []struct {
-		sli []int
-		i   int
-		out int
-		ok  bool
-	}{
-		{[]int{1, 2, 3}, 2, 3, true},
-		{[]int{1, 2, 3}, 3, 0, false},
-		{[]int{1, 2, 3}, 1, 2, true},
-		{[]int{1, 2, 3}, -1, 0, false},
-		{[]int{}, 0, 0, false},
-		{nil, 0, 0, false},
-	} {
-		out, ok := At(Iter(tc.sli), tc.i)
-		if out != tc.out || ok != tc.ok {
-			t.Errorf("Testcase %d: got (%v, %v), expect (%v, %v).", i, out, ok, tc.out, tc.ok)
-		}
-	}
-}
 
 func TestMap(t *testing.T) {
 	for i, tc := range []struct {
 		a   []int
-		f   func(int) string
+		f   func(int) (string, error)
 		out []string
+		err error
 	}{
-		{[]int{1, 2, 3, 4, 5}, func(a int) string { return strconv.Itoa(a + 1) }, []string{"2", "3", "4", "5", "6"}},
-		{nil, nil, nil},
-		{nil, func(a int) string { return "" }, nil},
+		{
+			[]int{1, 2, 3, 4, 5},
+			func(a int) (string, error) {
+				return strconv.Itoa(a + 1), nil
+			},
+			[]string{"2", "3", "4", "5", "6"},
+			nil,
+		},
+		{nil, nil, nil, nil},
+		{
+			nil,
+			func(a int) (string, error) {
+				return "", nil
+			},
+			nil,
+			nil,
+		},
 	} {
-		out := Collect(Map(Iter(tc.a), tc.f))
-		if !reflect.DeepEqual(out, tc.out) {
-			t.Errorf("Testcase %d: got %v, expect %v.", i, out, tc.out)
-		}
-	}
-}
-
-func TestScan(t *testing.T) {
-	for i, tc := range []struct {
-		sli []int
-		st  int
-		f   func(int, int) (int, bool)
-		out []int
-	}{
-		{[]int{1, 2, 3, 4, 5}, 0, func(st int, v int) (int, bool) { return st + v, true }, []int{1, 3, 6, 10, 15}},
-		{[]int{1, 2, 3, 4, 5}, 1, func(st int, v int) (int, bool) { return st + v, true }, []int{2, 4, 7, 11, 16}},
-		{nil, 0, nil, nil},
-	} {
-		out := Collect(Scan(Iter(tc.sli), tc.st, tc.f))
-		if !reflect.DeepEqual(out, tc.out) {
-			t.Errorf("Testcase %d: got %v, expect %v.", i, out, tc.out)
-		}
-	}
-}
-
-func TestReduce(t *testing.T) {
-	for i, tc := range []struct {
-		sli []int
-		f   func(int, int) int
-		out int
-		ok  bool
-	}{
-		{[]int{1, 2, 3, 4, 5}, func(a, b int) int { return a + b }, 15, true},
-		{[]int{1, 2, 3, 4, 5}, func(a, b int) int { return a * b }, 120, true},
-		{nil, nil, 0, false},
-	} {
-		out, ok := Reduce(Iter(tc.sli), tc.f)
-		if out != tc.out || ok != tc.ok {
-			t.Errorf("Testcase %d: got (%v, %v), expect (%v, %v).", i, out, ok, tc.out, tc.ok)
-		}
-	}
-}
-
-func TestFold(t *testing.T) {
-	for i, tc := range []struct {
-		sli []int
-		st  string
-		f   func(string, int) (string, bool)
-		out string
-	}{
-		{[]int{1, 2, 3, 4, 5}, "", func(st string, v int) (string, bool) { return st + strconv.Itoa(v), true }, "12345"},
-		{[]int{1, 2, 3, 4, 5}, "nyan", func(st string, v int) (string, bool) { return st + " " + strconv.Itoa(v), true }, "nyan 1 2 3 4 5"},
-		{nil, "", nil, ""},
-		{nil, "nyan", nil, "nyan"},
-	} {
-		out := Fold(Iter(tc.sli), tc.st, tc.f)
-		if out != tc.out {
-			t.Errorf("Testcase %d: got %v, expect %v.", i, out, tc.out)
-		}
-	}
-}
-
-func TestFilter(t *testing.T) {
-	for i, tc := range []struct {
-		sli []int
-		f   func(int) bool
-		out []int
-	}{
-		{[]int{1, 2, 3, 4, 5}, func(v int) bool { return v <= 3 }, []int{1, 2, 3}},
-		{[]int{1, 2, 3, 4, 5}, func(int) bool { return false }, nil},
-		{nil, nil, nil},
-	} {
-		out := Collect(Filter(Iter(tc.sli), tc.f))
-		if !reflect.DeepEqual(out, tc.out) {
-			t.Errorf("Testcase %d: got %v, expect %v.", i, out, tc.out)
-		}
-	}
-}
-
-func TestCount(t *testing.T) {
-	for i, tc := range []struct {
-		sli []int
-		out int
-	}{
-		{[]int{1, 2, 3, 4, 5}, 5},
-		{[]int{}, 0},
-		{nil, 0},
-	} {
-		out := Count(Iter(tc.sli))
-		if out != tc.out {
-			t.Errorf("Testcase %d: got %v, expect %v.", i, out, tc.out)
-		}
+		it := Iter(tc.a)
+		m := Map(it, tc.f)
+		out, err := Collect(m)
+		test.CheckEqual(t, i, "err", err, nil)
+		test.CheckDeepEqual(t, i, "out", out, tc.out)
 	}
 }
